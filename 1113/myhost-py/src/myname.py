1f99726e63e945da7bc3a1c65eafdfbd88b0fb34
SATO Daisuke <densuke@st.kobedenshi.ac.jp>
2020-11-12 14:31:48 +0900

    ホスト名を出るようにした

--- 1113/myhost-py/src/myname.py
+++ 1113/myhost-py/src/myname.py
@@ -2,6 +2,7 @@
 
 from bottle import route, run, response
 import json
+import socket
 
 @route("/")
 def index():
@@ -12,6 +13,17 @@ def index():
         "message": "index",
     }
     return json.dumps(ret)
+
+@route("/host")
+def get_myhost():
+    response.headers['Content-Type'] = 'application/json'
+    response.headers['Cache-Control'] = 'no-cache'
+    ret =  {
+        "path": "/host",
+        "message": socket.gethostname(),
+    }
+    return json.dumps(ret)
+
 
 if __name__ == "__main__":
     run(host="0.0.0.0", port=80)
